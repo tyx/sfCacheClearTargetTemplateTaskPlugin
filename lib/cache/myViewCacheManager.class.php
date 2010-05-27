692672c48e8fb0d627beb18573b6e5a271aa87d7
Timothée Barray <timothee@autrementlemail.com>
2010-05-27 14:19:54 +0200

    Initial commit

---
+++ lib/cache/myViewCacheManager.class.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This file is part of the .
+ * (c) 2010 Autrement
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * myCacheManager does things.
+ *
+ * @package    
+ * @subpackage 
+ * @author     Autrement
+ * @version    1.0.0
+ */
+class myViewCacheManager extends sfViewCacheManager
+{  
+  public function removeAppCache($paths, $app, $baseurl = '')
+  {
+    $this->cache->setOption('prefix', sfConfig::get('sf_apps_dir').'/'.$app.'/template:'.$baseurl);
+
+    foreach ($paths as $path)
+    {
+      if (strstr($path, '*') !== false)
+      {
+        $this->cache->removePattern($path);
+      }
+      else
+      {      
+        $this->cache->remove($this->generateCacheKey($path));
+      }
+    }
+  }
+}
---
+++ lib/task/sfCacheClearTargetTemplateTask.class.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * This file is part of the .
+ * (c) 2010 Autrement
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfCacheClearTargetTemplateTask.php does things.
+ *
+ * @package    
+ * @subpackage 
+ * @author     Autrement
+ * @version    1.0.0
+ */
+class sfCacheClearTargetTemplateTask extends sfCacheClearTask
+{
+  protected
+    $targetTemplate = null,
+    $baseurl = '';
+    
+  protected function configure()
+  {
+    $this->addArguments(array(
+      new sfCommandArgument('target', sfCommandArgument::REQUIRED, 'Target templates')
+    ));
+    
+    $this->addOptions(array(
+      new sfCommandOption('app', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', null),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_OPTIONAL, 'The environment', null),
+      new sfCommandOption('type', null, sfCommandOption::PARAMETER_OPTIONAL, 'The type', 'target'),
+      new sfCommandOption('baseurl', null, sfCommandOption::PARAMETER_OPTIONAL, 'Base URL', '/www_chambresapart_fr')
+    ));
+
+    $this->aliases = array('ctt');
+    $this->namespace = 'cache-target-template';
+    $this->name = 'clear';
+    $this->briefDescription = 'Clears the target template';
+
+    $this->detailedDescription = <<<EOF
+  	Remove target template
+  	
+EOF;
+  }
+  
+  protected function execute($arguments = array(), $options = array())
+  {
+    $this->targetTemplate = $arguments['target'];
+    $this->baseurl = $options['baseurl'];
+    
+    parent::execute($arguments, $options);
+  }
+  
+  protected function clearTargetCache(sfApplicationConfiguration $appConfiguration)
+  {
+    $config = $this->getFactoriesConfiguration($appConfiguration);
+		$context = sfContext::createInstance($appConfiguration);		
+
+    if (isset($config['view_cache']))
+    {
+      $class = $config['view_cache'];
+      $targets = explode(',', $this->targetTemplate);
+      $cache = new $class['class']($class['param']) ;
+      $cacheManager = new $config['view_cache_manager']['class']($context, $cache);
+      
+      $cacheManager->removeAppCache($targets, 'frontend', $this->baseurl);
+    }
+  }
+}
